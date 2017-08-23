fa07241b535619a67390010c9ef486e7e9204ada
Joey.Wang <loyalwong@gmail.com>
2017-08-23 14:33:08 +0800

    changes

--- appium_test03.py
+++ appium_test03.py
@@ -1,34 +1,28 @@
-import os
-from time import sleep
+#import os
+#from time import sleep
 from appium import webdriver
 
-import unittest
-
-# Returns abs path relative to this file and not cwd
-PATH = lambda p: os.path.abspath(
-    os.path.join(os.path.dirname(__file__), p)
-)
 
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['platformVersion'] = '6.0'
 desired_caps['deviceName'] = 'QMS4C15C28005345'
-desired_caps['app'] = PATH('guotai.apk'
-                           )
+desired_caps['udid'] = 'QMS4C15C28005345'
+desired_caps['appPackage'] = 'com.guotai.dazhihui'
+desired_caps['appActivity'] = 'com.android.dazhihui.view.screen.NewInitScreen'
+desired_caps['noReset'] = True
+desired_caps['fullReset'] = False
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
-el = driver.find_element_by_accessibility_id('Graphics')
-el.click()
-el = driver.find_element_by_accessibility_id('Arcs')
-driver.back()
-el = driver.find_element_by_accessibility_id("App")
-els = driver.find_elements_by_android_uiautomator("new UiSelector().clickable(true)")
-driver.find_element_by_android_uiautomator('text("API Demos")')
+#driver.tap([(860,1676)],500)
 
-el = driver.find_element_by_accessibility_id('Graphics')
+#我的
+el = driver.find_element_by_id('com.guotai.dazhihui:id/bottom_menu_button5')
 el.click()
-el = driver.find_element_by_accessibility_id('Arcs')
+#立即登录/注册
+el = driver.find_element_by_id('com.guotai.dazhihui:id/btn_usercenter_register')
 el.click()
-driver.find_element_by_android_uiautomator('new UiSelector().text("Graphics/Arcs")')
+
+driver.back()
 
 driver.quit()

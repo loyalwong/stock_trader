500d885e9b79f2185fb31b699fe61c25ed62df86
loyalwong <loyalwong@gmail.com>
2017-08-30 22:29:29 +0800

    changes

--- appium_test03.py
+++ appium_test03.py
@@ -9,19 +9,63 @@ desired_caps['platformVersion'] = '6.0'
 desired_caps['deviceName'] = 'QMS4C15C28005345'
 desired_caps['udid'] = 'QMS4C15C28005345'
 desired_caps['appPackage'] = 'com.guotai.dazhihui'
-desired_caps['appActivity'] = 'com.android.dazhihui.view.screen.NewMainScreen'
+desired_caps['appActivity'] = 'com.android.dazhihui.view.screen.NewInitScreen'
 desired_caps['noReset'] = True
 desired_caps['fullReset'] = False
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
-#driver.tap([(860,1676)],500)
+#警告框
+try:
+    elem1 = driver.find_element_by_id('com.guotai.dazhihui:id/layout_mainpage_nrng_icon')
+except Exception:
+    driver.tap([(860, 1676)], 500)
+else:
+    print("no alert need press")
 
 #我的
-el = driver.find_element_by_id('com.guotai.dazhihui:id/bottom_menu_button5')
-el.click()
+try:
+    elem2 = driver.find_element_by_id('com.guotai.dazhihui:id/bottom_menu_button5')
+except Exception:
+    print("error")
+else:
+    elem2.click()
+
 #立即登录/注册
-el = driver.find_element_by_id('com.guotai.dazhihui:id/btn_usercenter_register')
+try:
+    elem3 = driver.find_element_by_id('com.guotai.dazhihui:id/btn_usercenter_register')
+except Exception:
+    print("error")
+else:
+    elem3.click()
+
+#短信登录
+try:
+    elem3 = driver.find_element_by_id('com.guotai.dazhihui:id/tv_right')
+except Exception:
+    print("error")
+else:
+    elem3.click()
+
+try:
+    elem4 = driver.find_element_by_id('com.guotai.dazhihui:id/et_register_phonenum_smsmode')
+except Exception:
+    print("error")
+else:
+    elem4.clear()
+    elem4.send_keys("17765178235")
+    elem4.submit()
+    elem5 = driver.find_element_by_id('com.guotai.dazhihui:id/btn_register_get_verifycode')
+    elem5.click()
+    elem6 = driver.find_element_by_id('com.guotai.dazhihui:id/et_register_verify_code')
+    elem6.clear()
+    verify_code = input()
+    elem6.send_keys(verify_code)
+    elem6.submit()
+
+#交易
+el = driver.find_element_by_id('com.guotai.dazhihui:id/bottom_menu_button3')
 el.click()
+
 
 driver.back()
 
